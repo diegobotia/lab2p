d7d67a2fa4f3155416b3c734135f89180aceca8d
DB <diego.botia@udea.edu.co>
2025-10-06 14:51:23 -0500

    commit 1

---
+++ src/main/java/com/udea/lab2p/Lab2pApplication.java
@@ -0,0 +1,13 @@
+package com.udea.lab2p;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2pApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab2pApplication.class, args);
+	}
+
+}
